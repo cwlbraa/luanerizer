0e0b1e1bf905dd8e8a0d356c6a6e1bc984bfe696
Connor Braa <cbraa@pivotal.io>
2016-05-06 15:51:54 -0700

    make it all 12 factorish
    Signed-off-by: Connor Braa <cbraa@pivotal.io>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python luanerizer.py
--- luanerizer.py
+++ luanerizer.py
@@ -1,8 +1,50 @@
+from flask import Flask
+from flask import request
 import nltk
+import string
+import os
+
+app = Flask(__name__)
+
+NOUNTAGS = set(['PRP', 'NNP', 'NN', 'NNPS', 'NNS', 'NOUN'])
+LUAN = ':luan:'
+
+@app.route('/', methods=['POST'])
+def luanize():
+    dataIn = request.form['text']
+    luanized = luanizeTextBlock(dataIn)
+    return luanized
+
+def luanizeTextBlock(rawInput):
+    tokd = nltk.word_tokenize(rawInput.strip())
+    nouns = {noun[0] for noun in nltk.pos_tag(tokd, tagset="universal") if tagIsNoun(noun)}
+
+    def chunkIsNoun(chunk):
+        return chunk.strip(string.punctuation) in nouns
+
+    spaceDelimitedLines = [line.split() for line in rawInput.splitlines()]
+    result = ""
+    for line in spaceDelimitedLines:
+        luanizedWords = [luanizeWord(word) if chunkIsNoun(word)
+                         else word
+                         for word in line]
+
+        result += " ".join(luanizedWords) + "\n"
+
+    return result
+
+def tagIsNoun(taggedWord):
+    return taggedWord[1] in NOUNTAGS
+
+def luanizeWord(word):
+    wnl = nltk.stem.WordNetLemmatizer()
+    lemma = wnl.lemmatize(word.strip(string.punctuation))
+    return word.replace(lemma, LUAN)
+
 
 ozy = """I MET a Traveler from an antique land,
 Who said, "Two vast and trunkless legs of stone
-Stand in the desart. Near them, on the sand,
+Stand in the desert. Near them, on the sand,
 Half sunk, a shattered visage lies, whose frown,
 And wrinkled lip, and sneer of cold command,
 Tell that its sculptor well those passions read,
@@ -15,26 +57,6 @@ No thing beside remains. Round the decay
 Of that Colossal Wreck, boundless and bare,
 The lone and level sands stretch far away."""
 
-def scratch():
-    tokd = nltk.word_tokenize(ozy)
-    # print(nltk.pos_tag(tokd))
-    taggedWords = nltk.pos_tag(tokd)
-    luanizedWords = [luanizeNounLemma(taggedWord) for taggedWord in taggedWords]
-    # print(luanizedWords)
-    print(" ".join(luanizedWords))
-
-
-NOUNTAGS = set(['PRP', 'NNP', 'NN', 'NNPS', 'NNS'])
-def luanizeNounLemma(taggedWord):
-    if taggedWord[1] in NOUNTAGS:
-        return luanize(taggedWord[0])
-    return taggedWord[0]
-
-def luanize(word):
-    wnl = nltk.stem.WordNetLemmatizer()
-    lemma = wnl.lemmatize(word)
-    return word.replace(lemma, ':luan:')
-
-
-
-
+if __name__ == '__main__':
+    nltk.download('book')
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
