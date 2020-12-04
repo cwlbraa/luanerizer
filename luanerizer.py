0d52bf56d3adf375aca1509f3871b0dd781320c3
Connor Braa <cbraa@pivotal.io>
2020-12-03 17:17:50 -0800

    pylint & black compliant
    - better poetry integration to get nvim working
    
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- luanerizer.py
+++ luanerizer.py
@@ -1,48 +1,77 @@
-from flask import Flask, request, jsonify
-from flask import request
-import nltk
+"""
+ye olden luanerizer - a slack slash-command for luanerizing
+"""
 import string
 import os
+from flask import Flask, request, jsonify
+import nltk
 
 app = Flask(__name__)
 
-NOUNTAGS = set(['PRP', 'NNP', 'NN', 'NNPS', 'NNS', 'NOUN'])
-LUAN = os.environ.get('LUAN', ':luan:')
+NOUNTAGS = set(["PRP", "NNP", "NN", "NNPS", "NNS", "NOUN"])
+LUAN = os.environ.get("LUAN", ":luan:")
 
-@app.route('/luanize', methods=['POST'])
-def luanize():
-    dataIn = request.form['text']
-    luanized = luanizeTextBlock(dataIn)
-    return jsonify(text=luanized, response_type="in_channel")
 
-def luanizeTextBlock(rawInput):
-    tokd = nltk.word_tokenize(rawInput.strip())
-    nouns = {noun[0] for noun in nltk.pos_tag(tokd, tagset="universal") if tagIsNoun(noun)}
+@app.route("/luanize", methods=["POST"])
+def luanize_post():
+    """
+    takes a form-encoded "text" field and returns some json for the slackbot
+    """
+    return jsonify(text=luanize(request.form["text"]), response_type="in_channel")
 
-    def chunkIsNoun(chunk):
+
+def luanize(text):
+    """
+    takes text, finds the nouns, and turns them into luans
+    """
+    nouns = find_nouns(text)
+
+    def is_noun(chunk):
         return chunk.strip(string.punctuation) in nouns
 
-    spaceDelimitedLines = [line.split() for line in rawInput.splitlines()]
+    space_delimited_lines = [line.split() for line in text.splitlines()]
     result = ""
-    for line in spaceDelimitedLines:
-        luanizedWords = [luanizeWord(word) if chunkIsNoun(word)
-                         else word
-                         for word in line]
+    for line in space_delimited_lines:
+        luanized_words = [
+            luanize_word(word) if is_noun(word) else word for word in line
+        ]
 
-        result += " ".join(luanizedWords) + "\n"
+        result += " ".join(luanized_words) + "\n"
 
     return result
 
-def tagIsNoun(taggedWord):
-    return taggedWord[1] in NOUNTAGS
 
-def luanizeWord(word):
+def find_nouns(text):
+    """
+    returns all the nouns in the text
+    """
+    tokd = nltk.word_tokenize(text.strip())
+    nouns = {
+        word_with_tag[0]
+        for word_with_tag in nltk.pos_tag(tokd, tagset="universal")
+        if tagged_word_is_noun(word_with_tag)
+    }
+    return nouns
+
+
+def tagged_word_is_noun(word_with_tag):
+    """
+    returns true when a tagged word has a nouny part-of-speech
+    """
+    return word_with_tag[1] in NOUNTAGS
+
+
+def luanize_word(word):
+    """
+    replace the lemma (ie the non-modifier part of the word)
+    with luan
+    """
     wnl = nltk.stem.WordNetLemmatizer()
     lemma = wnl.lemmatize(word.strip(string.punctuation))
     return word.replace(lemma, LUAN)
 
 
-ozy = """I MET a Traveler from an antique land,
+OZY = """I MET a Traveler from an antique land,
 Who said, "Two vast and trunkless legs of stone
 Stand in the desert. Near them, on the sand,
 Half sunk, a shattered visage lies, whose frown,
@@ -57,5 +86,5 @@ No thing beside remains. Round the decay
 Of that Colossal Wreck, boundless and bare,
 The lone and level sands stretch far away."""
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")))
